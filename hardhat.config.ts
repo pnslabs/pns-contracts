a0413494c581fec80b893633a8c98e069c48d793
bizmindx <bizmindx@gmail.com>
2022-09-25 18:43:00 +0200

    fix broken compile pipeline

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.9",
+        version: '0.8.9',
       },
     ],
     optimizer: {
@@ -21,10 +21,19 @@ module.exports = {
     },
   },
   networks: {
+    hardhat: {
+      mining: {
+        auto: true,
+      },
+      // forking: {
+      //   url: 'https://mainnet.infura.io/v3/93c3c3bccee54144aa42c29ad05ef4f5',
+      //   blockNumber: 14622817,
+      // },
+    },
     localhost: {
       url: `http://127.0.0.1:8545`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-      gasPrice: parseInt(utils.parseUnits("132", "gwei")),
+      gasPrice: parseInt(utils.parseUnits('132', 'gwei')),
     },
   },
 };
