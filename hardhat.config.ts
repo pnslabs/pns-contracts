a38f9b9afec7f1124fb5092d2da3f5ae1764fe7e
meisterjustice <eziefulejustice@gmail.com>
2022-10-18 01:32:07 +0400

    chore: solve helper warning

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ module.exports = {
     localhost: {
       url: `http://127.0.0.1:8545`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-      gasPrice: parseInt(utils.parseUnits('132', 'gwei')),
+      gasPrice: parseInt(`${utils.parseUnits('132', 'gwei')}`),
     },
   },
 };
