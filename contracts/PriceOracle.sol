21c5cada67ae07af416993a093d1219c2577087c
bizmindx <bizmindx@gmail.com>
2022-11-26 15:55:51 +0100

    upgrade safe price oracle

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -5,28 +5,23 @@ pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+
 contract PriceOracle {
 
-    AggregatorV3Interface internal priceFeed;
-
-    /**
-     * Network: Goerli
-     * Aggregator: ETH/USD
-     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-     */
-    constructor() {
-	    /// Chainlink Oracle (Ether)
-        priceFeed = AggregatorV3Interface(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
-    }
+    //testnet priceFeed goreil
+    AggregatorV3Interface private constant ETH_USD_CHAINLINK = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
 
    /**
-     * @dev Returns the ETH price in DAI using chainlink
+     * @dev Returns the ETH price in USD ( DAI) using chainlink
      */
-    function getEtherPrice() internal view returns (uint256) {
-        (uint80 roundID, int256 price, , , uint80 answeredInRound) = priceFeed.latestRoundData();
+    function getEtherPriceInUSD() internal view returns (uint256) {
+        (uint80 roundID,
+         int256 price,
+          ,
+          ,
+         uint80 answeredInRound) = ETH_USD_CHAINLINK.latestRoundData();
         require(answeredInRound >= roundID, "getEtherPrice: Chainlink Price Stale");
-        require(price != 0, "getEtherPrice: Chainlink Malfunction");
         // Chainlink returns 8 decimal places so we convert
-        return uint256(price) * (10**10);
+        return uint256(price) * (10**8);
     }
 }
