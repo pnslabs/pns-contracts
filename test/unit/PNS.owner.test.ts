f86ceed2365abb81585d329cc0fd025b2f204cb6
Njoku Emmanuel <kalunjoku123@gmail.com>
2022-12-08 20:15:34 +0400

    chore:update tests 🧪

--- test/unit/PNS.owner.test.ts
+++ test/unit/PNS.owner.test.ts
@@ -6,17 +6,41 @@ const { deployContract } = require('../../scripts/deploy-helpers');
 
 describe('PNS Record Owner', () => {
   let pnsContract;
+  let pnsGuardianContract;
+  let adminAddress;
   const phoneNumber = keccak256('07084462591');
   const label = 'ETH';
   const address = '0xcD058D84F922450591AD59303AA2B4A864da19e6';
 
+  const status = true;
+  const signer = ethers.provider.getSigner();
+  const otp = '123456';
+
+  let message = ethers.utils.solidityPack(['bytes32', 'uint256'], [phoneNumber, otp]);
+  const hashedMessage = ethers.utils.keccak256(message);
+  let signature;
+
   before(async function () {
-    const { pnsContract: _pnsContract } = await deployContract();
+    signature = await signer.signMessage(ethers.utils.arrayify(hashedMessage));
+    const {
+      pnsContract: _pnsContract,
+      adminAddress: _adminAddress,
+      pnsGuardianContract: _pnsGuardianContract,
+    } = await deployContract();
     pnsContract = _pnsContract;
+    adminAddress = _adminAddress;
+    pnsGuardianContract = _pnsGuardianContract;
+  });
+
+  it('should verify the phone number', async () => {
+    await expect(pnsGuardianContract.setVerificationStatus(phoneNumber, hashedMessage, status, signature)).to.emit(
+      pnsGuardianContract,
+      'PhoneVerified',
+    );
   });
 
   it('should create a new record and emit an event', async function () {
-    await expect(pnsContract.setPhoneRecord(phoneNumber, address, address, label)).to.emit(
+    await expect(pnsContract.setPhoneRecord(phoneNumber, adminAddress, label)).to.emit(
       pnsContract,
       'PhoneRecordCreated',
     );
@@ -25,17 +49,17 @@ describe('PNS Record Owner', () => {
   it('gets the correct owner of the record', async () => {
     const recordOwner = await pnsContract.getOwner(phoneNumber);
 
-    assert.equal(recordOwner, address);
+    assert.equal(recordOwner, adminAddress);
   });
 
-  // it('changes record owner and emits an event', async () => {
-  //   await expect(
-  //     pnsContract.connect(address3).setOwner(phoneNumber1, adminAddress),
-  //   ).to.emit(pnsContract, 'Transfer');
-  // });
+  it('changes record owner and emits an event', async () => {
+    await expect(
+      pnsContract.setOwner(phoneNumber, address),
+    ).to.emit(pnsContract, 'Transfer');
+  });
 
-  // it('gets newly transfered record owner', async () => {
-  //   const recordOwner = await pnsContract.getOwner(phoneNumber1);
-  //   assert.equal(recordOwner, address1);
-  // });
+  it('gets newly transfered record owner', async () => {
+    const recordOwner = await pnsContract.getOwner(phoneNumber);
+    assert.equal(recordOwner, address);
+  });
 });
