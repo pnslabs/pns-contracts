d926fe635f0649c0db26a23f10303e5dfc82b2c4
bizmindx <bizmindx@gmail.com>
2022-12-10 13:54:25 +0100

    dummy oracle init

--- contracts/mocks/DummyPriceOracle.sol
+++ contracts/mocks/DummyPriceOracle.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.9;
 
-contract DummyOracle {
+contract DummyPriceOracle {
 	int256 value;
 
 	constructor(int256 _value) public {
