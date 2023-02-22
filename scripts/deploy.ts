b8a09761dc0a36752d982a495b0b274ac50a5c85
bizmindx <bizmindx@gmail.com>
2023-02-22 05:29:42 +0100

    fix ci

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -94,7 +94,7 @@ async function deployContract() {
       },
     );
   } else {
-    const treasuryAddress = 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266;
+    const treasuryAddress = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
     pnsRegistryContract = await upgrades.deployProxy(
       PNSRegistryContract,
       [pnsGuardianContract.address, dummyPriceOrcleContract.address, adminAddress, treasuryAddress],
