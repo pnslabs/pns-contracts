f2c7586ebd433e32da7a6a93fe4e637b6fc5c6c9
bizmindx <bizmindx@gmail.com>
2023-02-22 05:23:04 +0100

    mv to diff folders

---
+++ contracts/Interfaces/pns/IPNSGuardian.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+/**
+ * @title Interface for the PNS Guardian contract.
+ * @author PNS foundation core
+ * @notice This only serves as a function guide for the PNS Guardian.
+ * @dev All function call interfaces are defined here.
+ */
+interface IPNSGuardian {
+	/**
+	 * @dev logs the event when a phone record is verified.
+	 * @param owner The phoneHash to be linked to the record.
+	 * @param phoneHash The resolver (address) of the record
+	 * @param verifiedAt The address of the owner
+	 */
+	event PhoneVerified(address indexed owner, bytes32 indexed phoneHash, uint256 verifiedAt);
+
+	struct VerificationRecord {
+		uint256 verifiedAt;
+		bool isVerified;
+		address owner;
+	}
+
+	function getVerificationStatus(bytes32 phoneHash) external view returns (bool);
+
+	function getVerifiedOwner(bytes32 phoneHash) external view returns (address);
+
+	function getVerificationRecord(bytes32 phoneHash) external view returns (VerificationRecord memory);
+
+	function setVerificationStatus(
+		bytes32 phoneHash,
+		bool status,
+		bytes memory _signature
+	) external returns (bool);
+
+	function setGuardianVerifier(address _guardianVerifier) external;
+
+	function setPNSRegistry(address _registryAddress) external;
+}
---
+++ contracts/Interfaces/pns/IPNSRegistry.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+import './IPNSSchema.sol';
+
+/**
+ * @title Interface for the PNS Registry contract.
+ * @author PNS foundation core
+ * @notice This only serves as a function guide for the PNS Registry contract.
+ * @dev All function call interfaces are defined here.
+ */
+interface IPNSRegistry is IPNSSchema {
+	// event Transfer(bytes32 indexed phoneHash, address owner);
+
+	function setPhoneRecord(bytes32 phoneHash, string calldata resolver) external payable;
+
+	function setOwner(bytes32 phoneHash, address owner) external;
+
+	function getRecord(bytes32 phoneHash) external view returns (PhoneRecord memory);
+
+	// function getResolver(bytes32 phoneHash) external view returns (ResolverRecord[] memory);
+
+	function getRegistryCost() external view returns (uint256);
+
+	function getRegistryRenewCost() external view returns (uint256);
+
+	function setPhoneRecordMapping(PhoneRecord memory recordData, bytes32 phoneHash) external;
+
+	function renew(bytes32 phoneHash) external payable;
+
+	function getAmountinETH(uint256 usdAmount) external view returns (uint256);
+
+	function claimExpiredPhoneRecord(
+		bytes32 phoneHash,
+		address resolver,
+		string memory label
+	) external payable;
+
+	function setExpiryTime(uint256 time) external;
+
+	function isRecordVerified(bytes32 phoneHash) external view returns (bool);
+
+	function getExpiryTime() external view returns (uint256);
+
+	function getGracePeriod() external view returns (uint256);
+
+	function setGracePeriod(uint256 time) external;
+
+	function setRegistryCost(uint256 _registryCost) external;
+
+	function setRegistryRenewCost(uint256 _renewalCost) external;
+
+	function getRecordMapping(bytes32 phoneHash) external view returns (PhoneRecord memory);
+
+	function verifyPhone(
+		bytes32 phoneHash,
+		bytes32 hashedMessage,
+		bool status,
+		bytes memory signature
+	) external;
+
+	function setGuardianAddress(address guardianAddress) external;
+
+	function withdraw(address _recipient, uint256 amount) external;
+
+	function getVersion() external view returns (uint32 version);
+
+	function recordExists(bytes32 phoneHash) external view returns (bool);
+
+	// function getVerificationRecord(bytes32 phoneHash) external view returns (VerificationRecord memory);
+}
