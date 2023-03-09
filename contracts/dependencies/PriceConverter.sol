a23d868441605c6f97d25a8f855b283471837148
AmadiMichael <amadimichaeld@gmail.com>
2023-03-09 21:20:05 +0100

    reverted priceConverter back

--- contracts/dependencies/PriceConverter.sol
+++ contracts/dependencies/PriceConverter.sol
@@ -2,14 +2,17 @@
 pragma solidity 0.8.9;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+
+interface AggregatorInterface {
+	function latestAnswer() external view returns (int256);
+}
 
 contract PriceConverter is Ownable {
 	/// Oracle feed pricing
-	AggregatorV3Interface public priceFeedContract;
+	AggregatorInterface public priceFeedContract;
 
 	constructor(address _priceAggregator) {
-		priceFeedContract = AggregatorV3Interface(_priceAggregator);
+		priceFeedContract = AggregatorInterface(_priceAggregator);
 	}
 
 	/**
@@ -34,27 +37,15 @@ contract PriceConverter is Ownable {
 	 * @dev Returns the latest price
 	 */
 	function getEtherPriceInUSD() public view returns (uint256) {
-		(uint80 roundID, int256 answer, , uint256 updatedAt, uint80 answeredInRound) = priceFeedContract.latestRoundData();
-
-		// check that answer is indeed from the last known round
-		require(answeredInRound != roundID, 'Stale price');
-		// check that answer is within an allowed margin of freshness by checking updated at was updated less than an hour ago
-		require(updatedAt > block.timestamp - 3600, 'Answer is not from last known round');
-		// check returned answer is not zero
-		require(answer <= 0, 'Negative price');
-
-		// safe to cast answer into uint256 as we reqquire it to be greater than 0 or above and decimals returns a uint8
-		// this also assumes that aggregator decimals won't surpass 18 as it will revert with a panic error also solidity doesn't support negatuve exponents yet
-		return uint256(answer) * (10**(18 - priceFeedContract.decimals()));
+		int256 answer = priceFeedContract.latestAnswer();
+		// Chainlink returns 8 decimal places so we convert to wei
+		return uint256(answer * 1e10);
 	}
 
 	/**
 	 * @dev changes the price aggregator
 	 */
-	function changeAggregator(AggregatorV3Interface _priceFeedContract) external onlyOwner {
+	function changeAggregator(AggregatorInterface _priceFeedContract) external onlyOwner {
 		priceFeedContract = _priceFeedContract;
 	}
-
-	// important to prevent accidental renouncing
-	function renounceOwnership() public view override {}
 }
