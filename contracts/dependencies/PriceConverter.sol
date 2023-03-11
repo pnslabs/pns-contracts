2945a8fef819c00037272091ee0e8dd94dbe98bf
AmadiMichael <amadimichaeld@gmail.com>
2023-03-11 16:45:31 +0100

    fixed sign error in "getEtherPriceInUSD()"

--- contracts/dependencies/PriceConverter.sol
+++ contracts/dependencies/PriceConverter.sol
@@ -41,7 +41,7 @@ contract PriceConverter is Ownable {
 		// check that answer is within an allowed margin of freshness by checking updated at was updated less than an hour ago
 		require(updatedAt > block.timestamp - 3600, 'Answer is not from last known round');
 		// check returned answer is not zero
-		require(answer <= 0, 'Negative price');
+		require(answer >= 0, 'Negative price');
 
 		// safe to cast answer into uint256 as we require it to be greater than 0 or above and decimals returns a uint8
 		// this also assumes that aggregator decimals won't surpass 18 as it will revert with a panic error also solidity doesn't support negative exponents yet
