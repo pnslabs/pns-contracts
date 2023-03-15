df06330e4b582ae7d53899891b8fd8912c6bd6fb
AmadiMichael <amadimichaeld@gmail.com>
2023-03-15 08:16:36 +0100

    fix: in price converter

--- contracts/dependencies/PriceConverter.sol
+++ contracts/dependencies/PriceConverter.sol
@@ -37,11 +37,11 @@ contract PriceConverter is Ownable {
 		(uint80 roundID, int256 answer, , uint256 updatedAt, uint80 answeredInRound) = priceFeedContract.latestRoundData();
 
 		// check that answer is indeed from the last known round
-		require(answeredInRound != roundID, 'Stale price');
+		require(answeredInRound == roundID, 'Stale price');
 		// check that answer is within an allowed margin of freshness by checking updated at was updated less than an hour ago
 		require(updatedAt > block.timestamp - 3600, 'Answer is not from last known round');
 		// check returned answer is not zero
-		require(answer >= 0, 'Negative price');
+		require(answer > 0, 'Negative price');
 
 		// safe to cast answer into uint256 as we require it to be greater than 0 or above and decimals returns a uint8
 		// this also assumes that aggregator decimals won't surpass 18 as it will revert with a panic error also solidity doesn't support negative exponents yet
