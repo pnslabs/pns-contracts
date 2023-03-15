080d5a4919428d12490e3b50c73f32baebb0c04c
AmadiMichael <amadimichaeld@gmail.com>
2023-03-15 14:05:20 +0100

    gas optimizations via packing vars

--- contracts/PNSGuardian.sol
+++ contracts/PNSGuardian.sol
@@ -14,8 +14,6 @@ import './Interfaces/pns/IPNSRegistry.sol';
 /// @author  PNS core team
 /// @notice The PNS Guardian is responsible for authenticating the records created in PNS registry
 contract PNSGuardian is Initializable, IPNSGuardian, EIP712Upgradeable {
-	/// the guardian layer address that updates verification state
-	address public registryAddress;
 	/// PNS registry
 	IPNSRegistry public registryContract;
 	/// Address of off chain verifier
@@ -103,12 +101,16 @@ contract PNSGuardian is Initializable, IPNSGuardian, EIP712Upgradeable {
 
 		if (verificationRecord.owner == address(0)) {
 			verificationRecord.owner = signer;
-			verificationRecord.verifiedAt = block.timestamp;
+			verificationRecord.verifiedAt = uint48(block.timestamp);
 			verificationRecord.isVerified = status;
 		}
 
 		emit PhoneVerified(signer, phoneHash, block.timestamp);
 		return verificationRecord.owner != address(0);
+	}
+
+	function registryAddress() public view returns (address) {
+		return address(registryContract);
 	}
 
 	/**
