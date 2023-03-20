32e5c1150eb97584745cd55dd1f886fde6c2eb51
meisterjustice <eziefulejustice@gmail.com>
2023-03-20 14:10:29 +0400

    move resolver, guardian upgrade to UUPS

--- contracts/PNSGuardian.sol
+++ contracts/PNSGuardian.sol
@@ -6,7 +6,7 @@ import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol'
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
-// import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 
 //  ==========INTERNAL IMPORTS==========
 import './Interfaces/pns/IPNSGuardian.sol';
@@ -15,8 +15,7 @@ import './Interfaces/pns/IPNSRegistry.sol';
 /// @title Handles the authentication of the PNS registry
 /// @author  PNS core team
 /// @notice The PNS Guardian is responsible for authenticating the records created in PNS registry
-contract PNSGuardian is Initializable, IPNSGuardian, EIP712Upgradeable {
-
+contract PNSGuardian is Initializable, UUPSUpgradeable, IPNSGuardian, EIP712Upgradeable {
 	//============STATE VARIABLES==============
 
 	// PNS registry
@@ -32,66 +31,69 @@ contract PNSGuardian is Initializable, IPNSGuardian, EIP712Upgradeable {
 	//============EXTERNAL FUNCTIONS==============
 
 	/**
-	* @dev contract initializer function. This function exist because the contract is upgradable.
-	* @param _guardianVerifier Address to be stored as off-chain verifier
-	*/
+	 * @dev contract initializer function. This function exist because the contract is upgradable.
+	 * @param _guardianVerifier Address to be stored as off-chain verifier
+	 */
 	function initialize(address _guardianVerifier) external initializer {
 		// __Ownable_init();
 		__EIP712_init('PNS Guardian', '1.0');
 		guardianVerifier = _guardianVerifier;
 	}
 
+	/// @dev required by the OZ UUPS module
+	function _authorizeUpgrade(address) internal override onlyGuardianVerifier {}
+
 	/**
-	* @notice Gets the verification record for a phone hash
-	* @param phoneHash Hash of the phone number being verified
-	* @return VerificationRecord - Verification record associated with the phone hash
-	*/
+	 * @notice Gets the verification record for a phone hash
+	 * @param phoneHash Hash of the phone number being verified
+	 * @return VerificationRecord - Verification record associated with the phone hash
+	 */
 	function getVerificationRecord(bytes32 phoneHash) external view returns (VerificationRecord memory) {
 		VerificationRecord memory verificationRecord = verifiedRecord[phoneHash];
 		return verificationRecord;
 	}
 
 	/**
-	* @notice Gets the verification status for a phone hash
-	* @param phoneHash Hash of the phone number being verified
-	* @return bool - Verification status associated with the phone hash
-	*/
+	 * @notice Gets the verification status for a phone hash
+	 * @param phoneHash Hash of the phone number being verified
+	 * @return bool - Verification status associated with the phone hash
+	 */
 	function getVerificationStatus(bytes32 phoneHash) external view returns (bool) {
 		return verifiedRecord[phoneHash].isVerified;
 	}
 
 	/**
-	* @notice Gets the verified owner for a phone hash
-	* @param phoneHash Hash of the phone number being verified
-	* @return address - Verified owner associated with the phone hash
-	*/
+	 * @notice Gets the verified owner for a phone hash
+	 * @param phoneHash Hash of the phone number being verified
+	 * @return address - Verified owner associated with the phone hash
+	 */
 	function getVerifiedOwner(bytes32 phoneHash) external view returns (address) {
 		return verifiedRecord[phoneHash].owner;
 	}
 
 	/**
-	* @notice Sets the PNS registry address
-	* @param _registryAddress Address of the PNS registry
-	*/
+	 * @notice Sets the PNS registry address
+	 * @param _registryAddress Address of the PNS registry
+	 */
 	function setPNSRegistry(address _registryAddress) external onlyGuardianVerifier {
 		registryContract = IPNSRegistry(_registryAddress);
 	}
 
 	/**
-	* @notice Sets the guardian verifier address
-	* @param _guardianVerifier Address of the guardian verifier
-	*/
+	 * @notice Sets the guardian verifier address
+	 * @param _guardianVerifier Address of the guardian verifier
+	 */
 	function setGuardianVerifier(address _guardianVerifier) external onlyGuardianVerifier {
 		guardianVerifier = _guardianVerifier;
 	}
 
 	/**
-	* @notice Verifies a phone number hash
-	* @param phoneHash Hash of the phone number being verified
-	* @param status New verification status
-	* @param _signature Signature provided by the off-chain verifier
-	* @return bool - A boolean indicating if the verification record has been updated and is no longer a zero-address
-	*/
+	 * @notice Verifies a phone number hash
+	 * @param phoneHash Hash of the phone number being verified
+	 * @param status New verification status
+	 * @param _signature Signature provided by the off-chain verifier
+	 * @return bool - A boolean indicating if the verification record has been updated and is no longer a zero-address
+	 */
 	function verifyPhoneHash(
 		bytes32 phoneHash,
 		bytes32 _hashedMessage,
@@ -119,8 +121,8 @@ contract PNSGuardian is Initializable, IPNSGuardian, EIP712Upgradeable {
 	//============MODIFIERS==============
 
 	/**
-	* @dev Modifier that permits modifications only by the PNS guardian verifier.
-	*/
+	 * @dev Modifier that permits modifications only by the PNS guardian verifier.
+	 */
 	modifier onlyGuardianVerifier() {
 		require(msg.sender == guardianVerifier, 'Only Guardian Verifier');
 		_;
